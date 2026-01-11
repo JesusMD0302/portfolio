b28f0710cba7dc790ca29e1ba555a8ab036047f8
JesusMena <jesusalbertomena2002@gmail.com>
2026-01-10 23:10:49 -0600

    Add GitHub Actions workflow for deploying to GitHub Pages and update Astro config for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-    plugins: [tailwindcss()]
-  }
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://jesusmd0302.github.io/portfolio/",
+  base: "/portfolio/",
 });
